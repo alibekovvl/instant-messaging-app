4808f239e86abfaf57e26518b5199d9ca1dd747b
MSI <aidemiralibekov@mail.ru>
2025-06-12 17:46:22 +0300

    fixed: SignalR client connection errors

---
+++ InstantMessagingApp.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using InstantMessagingApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InstantMessagingApp.Infrastructure.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Message> Messages { get; set; }
+}
--- InstantMessagingApp.WebApi/Program.cs
+++ InstantMessagingApp.WebApi/Program.cs
@@ -1,13 +1,16 @@
 using InstantMessagingApp.Application.Interfaces;
+using InstantMessagingApp.Infrastructure.Data;
 using InstantMessagingApp.Infrastructure.Extentions;
 using InstantMessagingApp.Infrastructure.Repositories;
 using InstantMessagingApp.Infrastructure.Services;
 using InstantMessagingApp.Infrastructure.Services.SignalR;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddControllers();
 builder.Services.AddSignalR();
